9d1e964384ecdc7840212b0b07e9909fd3564081
AntonioJavierRP <antoniojrp1996@gmail.com>
2021-09-06 17:22:22 +0200

    Fixed typo

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -54,7 +54,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises={exercises3} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
